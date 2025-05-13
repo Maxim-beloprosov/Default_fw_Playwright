2aed42970d336c21551db2952a013d92904b1c6e
beloprosov <beloprosov@1221systems.com>
2025-05-13 14:48:53 +0300

    minor edits

--- fw/BrowserManager.py
+++ fw/BrowserManager.py
@@ -10,11 +10,10 @@ class BrowserManager:
         self.browser = None
 
     def get_browser(self):
-        if self.page is None:
-            self.playwright = sync_playwright().start()
-            self.browser = self.playwright.chromium.launch(headless=Settings.Browser['headless'],
-                                                           args=["--start-maximized"])
-            self.page = self.browser.new_page(no_viewport=True)
+        self.playwright = sync_playwright().start()
+        self.browser = self.playwright.chromium.launch(headless=Settings.Browser['headless'],
+                                                       args=["--start-maximized"])
+        self.page = self.browser.new_page(no_viewport=True)
         return self.page
 
     def stop_browser(self):
--- fw/web/web_fw_base.py
+++ fw/web/web_fw_base.py
@@ -1,3 +1,5 @@
+import time
+
 from fw.fw_base import FWBase
 
 import allure
@@ -25,15 +27,20 @@ class WebFwBase(FWBase):
 
     @allure.step('GoTo URL - {page}')
     def goto_url(self, page):
-        current_url = self.get_current_url()
-        # Если текущий URL не совпадает с переданным, переходим по новому URL
-        if page not in current_url:
-            self.get_page().goto(page)
+        self.get_page().goto(page, wait_until='load')
+        self.wait_until_page_is_ready(self.get_page())
+
+    @allure.step('Wait until page is fully loaded and ready')
+    def wait_until_page_is_ready(self, page):
+        # Ждёт полной загрузки всех сетевых запросов
+        page.wait_for_load_state('networkidle')
 
     @allure.step('Click')
     def click_element(self, locator):
         try:
-            self.get_page().click(locator)
+            page = self.get_page()
+            self.wait_until_page_is_ready(page)
+            page.click(locator)
 
         except TimeoutError as e:
             self.allure_TimeoutError(e)
@@ -43,7 +50,25 @@ class WebFwBase(FWBase):
     @allure.step('Send keys')
     def send_keys(self, locator, text):
         try:
-            self.get_page().fill(locator, text)
+            page = self.get_page()
+            self.wait_until_page_is_ready(page)
+            page.fill(locator, text)
+
+        except TimeoutError as e:
+            self.allure_TimeoutError(e)
+        except Exception as e:
+            self.allure_GenericError(e)
+
+    @allure.step('Clear and send keys')
+    def clear_and_send_keys(self, locator, text):
+        try:
+            page = self.get_page()
+            self.wait_until_page_is_ready(page)
+            input_field = page.locator(locator)
+            # Remove text from field
+            input_field.fill("a")
+            input_field.fill("")
+            input_field.type(text)
 
         except TimeoutError as e:
             self.allure_TimeoutError(e)
@@ -53,7 +78,9 @@ class WebFwBase(FWBase):
     @allure.step('Clear and send keys slow')
     def clear_and_send_keys_slow(self, locator, text, delay=100):
         try:
-            input_field = self.get_page().locator(locator)
+            page = self.get_page()
+            self.wait_until_page_is_ready(page)
+            input_field = page.locator(locator)
             # Remove text from field
             input_field.fill("")
             input_field.type(text, delay=delay)
@@ -66,7 +93,9 @@ class WebFwBase(FWBase):
     @allure.step('Get input value from field')
     def get_input_value(self, locator):
         try:
-            input_value = self.get_page().locator(locator).input_value()
+            page = self.get_page()
+            self.wait_until_page_is_ready(page)
+            input_value = page.locator(locator).input_value()
             return input_value
 
         except TimeoutError as e:
@@ -77,7 +106,9 @@ class WebFwBase(FWBase):
     @allure.step('Get text from field')
     def get_tag_text(self, locator):
         try:
-            text = self.get_page().locator(locator).text_content()
+            page = self.get_page()
+            self.wait_until_page_is_ready(page)
+            text = page.locator(locator).text_content()
             return text
 
         except TimeoutError as e:
@@ -88,7 +119,9 @@ class WebFwBase(FWBase):
     @allure.step('Get tag attribute from field')
     def get_tag_attribute(self, locator, attribute_name):
         try:
-            tag_attribute = self.get_page().locator(locator).get_attribute(attribute_name)
+            page = self.get_page()
+            self.wait_until_page_is_ready(page)
+            tag_attribute = page.locator(locator).get_attribute(attribute_name)
             return tag_attribute
 
         except TimeoutError as e:
@@ -98,7 +131,9 @@ class WebFwBase(FWBase):
 
     @allure.step('Get tag attribute from field')
     def get_tag_attributes(self, locator, attribute_name):
-        tag_attributes = self.get_page().locator(locator)
+        page = self.get_page()
+        self.wait_until_page_is_ready(page)
+        tag_attributes = page.locator(locator)
         count = tag_attributes.count()
 
         list_tag_attributes = []
@@ -112,7 +147,9 @@ class WebFwBase(FWBase):
     @allure.step('Clear text from field')
     def clear_keys(self, locator):
         try:
-            input_field = self.get_page().locator(locator)
+            page = self.get_page()
+            self.wait_until_page_is_ready(page)
+            input_field = page.locator(locator)
             input_field.fill("")
 
         except TimeoutError as e:
@@ -121,12 +158,16 @@ class WebFwBase(FWBase):
             self.allure_GenericError(e)
 
     def get_current_url(self):
-        return self.get_page().url
+        page = self.get_page()
+        self.wait_until_page_is_ready(page)
+        return page.url
 
     @allure.step('Right click')
     def right_click(self, locator):
         try:
-            self.get_page().locator(locator).click(button="right")
+            page = self.get_page()
+            self.wait_until_page_is_ready(page)
+            page.locator(locator).click(button="right")
 
         except TimeoutError as e:
             self.allure_TimeoutError(e)
@@ -136,7 +177,9 @@ class WebFwBase(FWBase):
     @allure.step('Double click')
     def double_click(self, locator):
         try:
-            self.get_page().dblclick(locator)
+            page = self.get_page()
+            self.wait_until_page_is_ready(page)
+            page.dblclick(locator)
 
         except TimeoutError as e:
             self.allure_TimeoutError(e)
@@ -146,7 +189,9 @@ class WebFwBase(FWBase):
     @allure.step('Refresh page')
     def refresh_the_page(self):
         try:
-            self.get_page().reload()
+            page = self.get_page()
+            self.wait_until_page_is_ready(page)
+            page.reload()
 
         except TimeoutError as e:
             self.allure_TimeoutError(e)
@@ -154,7 +199,9 @@ class WebFwBase(FWBase):
     @allure.step('Send keys slow')
     def send_keys_slow(self, locator, text, delay=100):
         try:
-            input_field = self.get_page().locator(locator)
+            page = self.get_page()
+            self.wait_until_page_is_ready(page)
+            input_field = page.locator(locator)
             input_field.type(text, delay=delay)
 
         except TimeoutError as e:
@@ -163,19 +210,25 @@ class WebFwBase(FWBase):
             self.allure_GenericError(e)
 
     @allure.step('Find element')
-    def find_element(self, locator):
+    def find_element(self, locator, wait=None):
+        if wait is None:
+            wait = self.manager.settings.time_element_wait
+        page = self.get_page()
+        self.wait_until_page_is_ready(page)
+        web_element = page.locator(locator)
         try:
-            web_element = self.get_page().locator(locator)
+            web_element.wait_for(state="visible", timeout=wait*1000)
             return web_element
-
-        except:
+        except TimeoutError:
             self.allure_screenshot()
-            raise
+            raise Exception(f"Элемент '{locator}' не появился за {wait} с")
 
     @allure.step('Find elements')
     def find_elements(self, locator):
         try:
-            web_elements = self.get_page().locator(locator)
+            page = self.get_page()
+            self.wait_until_page_is_ready(page)
+            web_elements = page.locator(locator)
             return web_elements
 
         except:
@@ -193,3 +246,38 @@ class WebFwBase(FWBase):
         self.allure_screenshot()
         print(f"GenericError occurred: {exception_text}")
         assert True == False  # Останавливаем тест
+
+    @allure.step('Проверка дизейбла нужного поля')
+    def check_disable_needed_field(self, locator):
+        # Получаем данные атрибута disabled
+        if (self.get_tag_attribute(locator, 'disabled') == ''
+                or self.get_tag_attribute(locator, 'readonly') == ''):
+            return True
+        else:
+            return False
+
+    @allure.step('Проверка валидации в нужном поле')
+    def check_validation_in_needed_field(self, locator, text):
+        if text == self.get_tag_text(locator):
+            return True
+        else:
+            return False
+
+    @allure.step('Ожидаем, пока данные в нужном поле прогрузятся')
+    def wait_while_data_in_needed_field_will_be_loaded(self, locator, empty_value=False, time_for_wait=2):
+        """
+        ОЖИДАЕТСЯ ПОДАЧА ЭЛЕМЕНТА INPUT
+        """
+        count = 0
+        if empty_value == True:
+            while self.get_tag_attribute(locator, 'value') is None:
+                time.sleep(0.1)
+                count = count + 0.1
+                if count >= time_for_wait:
+                    raise
+        else:
+            while self.get_tag_attribute(locator, 'value') is None or self.get_tag_attribute(locator, 'value') == '':
+                time.sleep(0.1)
+                count = count + 0.1
+                if count >= time_for_wait:
+                    raise
